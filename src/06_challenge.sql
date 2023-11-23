ef6ac610657026bb6eb5bb05b3afa15de95137ed
Vitor Cardoso <vitor.mauricioc@outlook.com>
2023-11-22 23:30:26 -0300

    adiciona sexta query

---
+++ src/06_challenge.sql
@@ -0,0 +1,4 @@
+-- Escreva uma query que aplica os apelidos `primeiro_nome` e `ultimo_nome` 
+-- respectivamente para as colunas `first_name` e `last_name` da tabela `users`;
+SELECT first_name AS primeiro_nome, last_name AS ultimo_nome
+FROM hotel_database.users
